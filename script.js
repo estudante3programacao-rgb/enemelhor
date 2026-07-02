36efaeb836504dfb542a0a5f2331d0c66ad477ef
estudante3programacao-rgb <estudante3programacao@gmail.com>
2026-07-02 20:46:37 -0300

    Refactor finalizarQuiz by removing unnecessary styles
    Removed styling properties from finalizarQuiz function.

--- script.js
+++ script.js
@@ -124,12 +124,7 @@ function atualizarEstrelas() {
 
 function finalizarQuiz() {
   document.getElementById("pergunta").textContent = "🏁 Enemelhor finalizado!Continue a estudar conosco!";
-  pergunta.style.color = "green";
-  pergunta.style.fontSize = "5rem";
-  pergunta.style.fontWeight = "bold";
-  pergunta.style.textAlign = "center";
-  pergunta.style.lineHeight = "1.5";
-  
+   
   document.querySelector(".teste-botoes").style.display = "none";
 }
 
