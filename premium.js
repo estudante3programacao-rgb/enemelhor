f1f04fb5f926a385cd1ed3d94adaf5e66c0f54d6
estudante3programacao-rgb <estudante3programacao@gmail.com>
2026-07-10 11:28:36 -0300

    Fix text formatting and punctuation in premium.js

--- premium.js
+++ premium.js
@@ -40,12 +40,12 @@ const perguntasPremium = [
     dificuldade:  "media"
   },
   {
-    texto: "Saudado por centenas de militantes de movimentos sociais de quarenta países, o papa Francisco encerrou no dia 09/07/2015 o 2º Encontro Mundial dos Movimentos Populares, em Santa Cruz de La Sierra, na Bolívia. Segundo ele, a “globalização da esperança, que nasce dos povos e cresce entre os pobres, deve substituir esta globalização da exclusão e da indiferença”.(Disponível em: http://cartamaior.com.br. Acesso em: 15 jul. 2015 (adaptado).) No texto há uma crítica ao seguinte aspecto do mundo globalizado: disparidade econômica.",
+    texto: "Saudado por centenas de militantes de movimentos sociais de quarenta países, o papa Francisco encerrou no dia 09/07/2015 o 2º Encontro Mundial dos Movimentos Populares, em Santa Cruz de La Sierra, na Bolívia. Segundo ele, a “globalização da esperança, que nasce dos povos e cresce entre os pobres, deve substituir esta globalização da exclusão e da indiferença”. (Disponível em: http://cartamaior.com.br. Acesso em: 15 jul. 2015 (adaptado)). No texto há uma crítica ao seguinte aspecto do mundo globalizado: disparidade econômica.",
     resposta: true,
     dificuldade:  "media"
   },
   {
-    texto: "O bônus demográfico é caracterizado pelo período em que, por causa da redução do número de filhos  por mulher, a estrutura populacional fica favorável  ao crescimento econômico. Isso acontece porque há  proporcionalmente menos crianças na população, e o  percentual de idosos ainda não é alto.(GOIS, A. O Globo, 5 abr. 2015 (adaptado).) A ação estatal que contribui para o aproveitamento do  bônus demográfico é o estímulo à qualificação da mão de obra.", 
+    texto: "O bônus demográfico é caracterizado pelo período em que, por causa da redução do número de filhos por mulher, a estrutura populacional fica favorável ao crescimento econômico. Isso acontece porque há proporcionalmente menos crianças na população, e o percentual de idosos ainda não é alto. (GOIS, A. O Globo, 5 abr. 2015 (adaptado)). A ação estatal que contribui para o aproveitamento do bônus demográfico é o estímulo à qualificação da mão de obra.", 
     resposta: true,
     dificuldade:  "media"
   },
@@ -58,113 +58,113 @@ const perguntasPremium = [
     texto: "Art. 231. São reconhecidos aos índios sua organização social, costumes, línguas, crenças e tradições, e os direitos originários sobre as terras que tradicionalmente ocupam, competindo à União demarcá-las, proteger e fazer respeitar todos os seus bens. (BRASIL. Constituição da República Federativa do Brasil de 1988.) A persistência das reivindicações relativas à aplicação desse preceito normativo tem em vista a vinculação histórica e fundamental entre progresso e educação ambiental.",
     resposta: false,
     dificuldade: "fácil"
-},
+  },
   {
     testo: "Muitos países se caracterizam por terem populações multiétnicas. Com frequência, evoluíram desse modo ao longo de séculos. Outras sociedades se tornaram multiétnicas mais rapidamente, como resultado de políticas incentivando a migração, ou por conta de legados coloniais e imperiais (GIDDENS, A. Sociologia. Porto Alegre: Penso, 2012 (adaptado)). Do ponto de vista do funcionamento das democracias contemporâneas, o modelo de sociedade descrito demanda, simultaneamente padronização da cultura e repressão aos particularismos.",
     resposta: false,
     dificuldade: "fácil"
   },
   {
-    texto: "Com a Lei de Terras de 1850, o acesso à terra só passou a ser possível por meio da compra com pagamento em dinheiro. Isso limitava, ou mesmo praticamente impedia, o acesso à terra para os trabalhadores escravos que conquistavam a liberdade  (OLIVEIRA, A. U. Agricultura brasileira: transformações recentes. In: ROSS, J. L. S. Geografia do Brasil. São Paulo: Edusp, 200). O fato legal evidenciado no texto acentuou o processo de reforma agrária",
+    texto: "Com a Lei de Terras de 1850, o acesso à terra só passou a ser possível por meio da compra com pagamento em dinheiro. Isso limitava, ou mesmo praticamente impedia, o acesso à terra para os trabalhadores escravos que conquistavam a liberdade. (OLIVEIRA, A. U. Agricultura brasileira: transformações recentes. In: ROSS, J. L. S. Geografia do Brasil. São Paulo: Edusp, 200). O fato legal evidenciado no texto acentuou o processo de reforma agrária",
     resposta: false,
     dificuldade: "fácil"
   },
-   {
-    texto: "O leite UHT (do inglês Ultra-High Temperature) é o leite tratado termicamente por um processo que recebe o nome de ultrapasteurização. Elevando sua temperatura homogeneamente a 135 °C por apenas 1 ou 2 segundos, o leite é esterilizado sem prejudicar significativamente seu sabor e aparência. Desse modo, ele pode ser armazenado, sem a necessidade de refrigeração, por meses. Para alcançar essa temperatura sem que a água que o compõe vaporize, o leite é aquecido em alta pressão. É necessário, entretanto, resfriar o leite rapidamente para evitar o seu cozimento. Para tanto, a pressão é reduzida subitamente, de modo que parte da água vaporize e a temperatura diminua. O processo termodinâmico que explica essa redução súbita de temperatura é a transferência de energia térmica durante a vaporização da água presente no leite ", 
+  {
+    texto: "O leite UHT (do inglês Ultra-High Temperature) é o leite tratado termicamente por um processo que recebe o nome de ultrapasteurização. Elevando sua temperatura homogeneamente a 135 °C por apenas 1 ou 2 segundos, o leite é esterilizado sem prejudicar significativamente seu sabor e aparência. Desse modo, ele pode ser armazenado, sem a necessidade de refrigeração, por meses. Para alcançar essa temperatura sem que a água que o compõe vaporize, o leite é aquecido em alta pressão. É necessário, entretanto, resfriar o leite rapidamente para evitar o seu cozimento. Para tanto, a pressão é reduzida subitamente, de modo que parte da água vaporize e a temperatura diminua. O processo termodinâmico que explica essa redução súbita de temperatura é a transferência de energia térmica durante a vaporização da água presente no leite.", 
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "No Autódromo de Interlagos, um carro de Fórmula 1 realiza a curva S do Senna numa trajetória curvilínea. Enquanto percorre esse trecho, o velocímetro do carro indica velocidade constante. Quais são a direção e o sentido da aceleração do carro? Radial, apontada para dentro da curva.",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "O que é software livre. Software livre é qualquer programa de computador construído de forma colaborativa, via internet, por uma comunidade internacional de desenvolvedores independentes. São centenas de milhares de hackers, que negam sua associação com os “violadores de segurança”. Esses desenvolvedores de software se recusam a reconhecer o significado pejorativo do termo e continuam usando a palavra hacker para indicar “alguém que ama programar e que gosta de ser hábil e engenhoso”. Além disso, esses programas são entregues à comunidade com o código fonte  aberto e disponível, permitindo que a ideia original possa ser aperfeiçoada e devolvida novamente à comunidade. Nos programas convencionais, o código de programação é secreto e de propriedade da empresa que o desenvolveu,  sendo quase impossível decifrar a programação. O que está em jogo é o controle da inovação tecnológica. Software livre é uma questão de liberdade de expressão e não apenas uma relação econômica. Hoje existem milhares de programas alternativos construídos dessa forma e uma comunidade de usuários com milhões de membros no mundo. (RANCO, M. Software livre e desenvolvimento social e económico. In: CASTELLS, M.; CARDOSO, G. (Org). A sociedade em rede: do conhecimento à acção política. Lisboa: Imprensa Nacional, 2005 (adaptado)). A criação de softwares livres contribui para a produção do conhecimento na sociedade porque insere profissionalmente os hackers na área de inovação tecnológica",
     resposta: false,
     dificuldade: "fácil"
   },
-   {
+  {
+    texto: "A rede é, antes de tudo, um instrumento de comunicação entre pessoas, um laço virtual em que as comunidades auxiliam seus membros a aprender o que querem saber. Os dados não representam senão a matéria-prima de um processo intelectual e social vivo, altamente elaborado. Enfim, toda inteligência coletiva do mundo jamais dispensará a inteligência pessoal, o esforço individual e o tempo necessário para aprender, pesquisar, avaliar e integrar-se a diversas comunidades, sejam elas virtuais ou não. A rede jamais pensará em seu lugar, fique tranquilo. (LÉVY, P. A máquina universo: criação, cognição e cultura informática. Porto Alegre: Artmed, 1998.). No contexto das novas tecnologias de informação e comunicação, a circulação de saberes depende da contribuição dos usuários.", 
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "No Brasil, a disseminação de uma expectativa de corpo com base na estética da magreza é bastante grande e apresenta uma enorme repercussão, especialmente, se considerada do ponto de vista da realização pessoal. Em pesquisa feita na cidade de São Paulo, aparecem os percentuais de 90% entre as mulheres pesquisadas que se dizem preocupadas com seu peso corporal, sendo que 95% se sentem insatisfeitas com “seu próprio corpo”. (SILVA, A. M. Corpo, ciência e mercado: reflexões acerca da gestação de um novo arquétipo da felicidade. Campinas: Autores Associados; Florianópolis: UFSC, 2001.). A preocupação excessiva com o “peso” corporal pode provocar o desenvolvimento de distúrbios associados diretamente à imagem do corpo, tais como anorexia e bulimia.",
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "Dizem que Humboldt, naturalista do século XIX, maravilhado pela geografia, flora e fauna da região sul-americana, via seus habitantes como se fossem mendigos sentados sobre um saco de ouro, referindo-se a suas incomensuráveis riquezas naturais não exploradas. De alguma maneira, o cientista ratificou nosso papel de exportadores de natureza no que seria o mundo depois da colonização ibérica: enxergou-nos como territórios condenados a aproveitar os recursos naturais existentes. (ACOSTA, A. Bem viver: uma oportunidade para imaginar outros mundos. São Paulo: Elefante, 2016 (adaptado)). A relação entre ser humano e natureza ressaltada no texto refletia a permanência da seguinte corrente filosófica Existencialismo fenomenológico",          
+    resposta: false,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "Em sentido geral e fundamental, Direito é a técnica da coexistência humana, isto é, a técnica voltada a tornar possível a coexistência dos homens. Como técnica, o Direito se concretiza em um conjunto de regras (que, nesse caso, são leis ou normas); e tais regras têm por objeto o comportamento intersubjetivo, isto é, o comportamento recíproco dos homens entre si. (BBAGNANO, N. Dicionário de Filosofia. São Paulo: Martins Fontes, 2007). O sentido geral e fundamental do Direito, conforme foi destacado, refere-se à regulação do convívio social.",
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "Reflorestamento é uma ação ambiental que visa repovoar áreas que tiveram a vegetação removida. Uma empresa deseja fazer um replantio de árvores e dispõe de cinco produtos que podem ser utilizados para corrigir o pH do solo que se encontra básico. As substâncias presentes nos produtos disponíveis são: CH3COONa, NH4Cl, NaBr, NaOH e KCl. A substância a ser adicionada ao solo para neutralizá-lo é NH4Cl.",
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "Fenômenos epigenéticos levam a modificações do DNA e das histonas, que influenciam o remodelamento da cromatina e, consequentemente, a disponibilização ou não de genes para a transcrição. (ARRUDA, I. T. S. Epigenética. Genética na Escola, n. 1, 2015 (adaptado)). Esses fenômenos atuam na regulação da expressão gênica.",
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
+    texto: "Nas últimas décadas vários países, inclusive o Brasil, têm testemunhado uma grande proliferação de bactérias patogênicas, envolvidas em uma variedade de doenças e que apresentam resistência a múltiplos antibióticos. Atualmente têm se destacado as superbactérias que acumularam vários genes determinantes de resistência, a ponto de se tornarem resistentes a praticamente todos os antimicrobianos. (ERREIRA, F. A.; CRUZ, R. S.; FIGUEIREDO, A. M. S. O problema da resistência a antibióticos. Ciência Hoje, v.48, n.287, 2011 (adaptado)). Essa resistência tem ocorrido porque os antibióticos são utilizados pela população de maneira indiscriminada.",
+    resposta: true,
+    dificuldade: "fácil"
+  },
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
   },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
-    texto: "",
-    resposta: true,
-    dificuldade: "fácil"
-  },
-   {
+  {
     texto: "",
     resposta: true,
     dificuldade: "fácil"
