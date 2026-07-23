6a0ba83978d41fcda1c0f2f64684ff9e62dd29b1
estudante3programacao-rgb <estudante3programacao@gmail.com>
2026-07-23 19:12:26 -0300

    Change resposta from false to true in premium.js

--- premium.js
+++ premium.js
@@ -846,7 +846,7 @@ const perguntasPremium = [
   },
   {
     texto: "Blog é concebido como um espaço onde o blogueiro é livre para expressar e discutir o que quiser na atividade da sua escrita, com a escolha de imagens e sons que compõem o todo do texto veiculado pela internet, por  meio dos posts. Assim, essa ferramenta deixa de ter como única função a exposição de vida e/ou rotina de  alguém — como em um diário pessoal —, função para qual serviu inicialmente e que o popularizou, permitindo também que seja um espaço para a discussão de ideias, trocas e divulgação de informações. <p>A produção dos blogs requer uma relação de troca, que acaba unindo pessoas em torno de um ponto de interesse comum. A força dos blogs está em possibilitar que qualquer pessoa, sem nenhum conhecimento técnico, publique suas ideias e opiniões na web e que milhões de outras pessoas publiquem comentários sobre o que foi escrito, criando um grande debate aberto a todos.</p>(LOPES, B. O. A linguagem dos blogs e as redes sociais. Disponível em: www.fateczl.edu.br. Acesso em: 29 abr. 2013. Adaptado). De acordo com o texto, o blog ultrapassou sua função inicial e vem se destacando como: espaço para exposição de opiniões e circulação de ideias.",    
-    resposta: false,
+    resposta: true,
     dificuldade: "fácil"
   },
   {
