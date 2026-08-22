1290cf9a12f266d8e0ddaac9cee4ebde7fbfde00
Maicon dos Santos Gonçalves <estudante3programacao@gmail.com>
2026-08-22 09:18:01 -0300

    Fix HTML 299 formatting in premium.js text content

--- premium.js
+++ premium.js
@@ -1490,7 +1490,7 @@ const perguntasPremium = [
     dificuldade: "fácil"
   },
   {
-    texto: "<p>Compreende-se assim o alcance de uma reivindicação que surge desde o nascimento da cidade na Grécia antiga: a redação das leis. Ao escrevê-las, não se faz mais que assegurar-lhes permanência e fixidez. As leis tornam-se bem comum, regra geral, suscetível de ser aplicada a todos da mesma maneira. (VERNANT, J. P. As origens do pensamento grego. Rio de Janeiro: Bertrand Brasil, 1992. Adaptado).</p><p>Para o autor, a reivindicação atendida na Grécia antiga, ainda vigente no mundo contemporâneo, buscava garantir o seguinte princípio: tripartição — separação entre os poderes políticos estatais.</p>",
+    texto: "<p>Compreende-se assim o alcance de uma reivindicação que surge desde o nascimento da cidade na Grécia antiga: a redação das leis. Ao escrevê-las, não se faz mais que assegurar-lhes permanência e fixidez. As leis tornam-se bem comum, regra geral, suscetível de ser aplicada a todos da mesma maneira.</p><p>(VERNANT, J. P. As origens do pensamento grego. Rio de Janeiro: Bertrand Brasil, 1992. Adaptado)</p><p>Para o autor, a reivindicação atendida na Grécia antiga, ainda vigente no mundo contemporâneo, buscava garantir o seguinte princípio: tripartição — separação entre os poderes políticos estatais.</p>",
     resposta: false,
     dificuldade: "fácil"
   },
