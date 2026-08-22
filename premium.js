af0a6c07462191f06c0787954927b360d00784ed
Maicon dos Santos Gonçalves <estudante3programacao@gmail.com>
2026-08-22 09:19:15 -0300

    Fix text 300 formatting in premium.js

--- premium.js
+++ premium.js
@@ -1495,7 +1495,7 @@ const perguntasPremium = [
     dificuldade: "fácil"
   },
   {
-    texto: "<p>Na década de 1940, na Região Centro-Oeste, produtores rurais, cujos bois, porcos, aves e cabras estavam morrendo por uma peste desconhecida, fizeram uma promessa, que consistiu em não comer carne e derivados até que a peste fosse debelada. Assim, durante três meses, arroz, feijão, verduras e legumes formaram o prato principal desses produtores. O Hoje, 15 out. 2011 (adaptado).</p><p>Para suprir o déficit nutricional a que os produtores rurais se submeteram durante o período da promessa, foi importante eles terem consumido alimentos ricos em: aminoácidos essenciais.</p>",
+    texto: "<p>Na década de 1940, na Região Centro-Oeste, produtores rurais, cujos bois, porcos, aves e cabras estavam morrendo por uma peste desconhecida, fizeram uma promessa, que consistiu em não comer carne e derivados até que a peste fosse debelada. Assim, durante três meses, arroz, feijão, verduras e legumes formaram o prato principal desses produtores.</p><p>(O Hoje, 15 out. 2011. Adaptado)</p><p>Para suprir o déficit nutricional a que os produtores rurais se submeteram durante o período da promessa, foi importante eles terem consumido alimentos ricos em: aminoácidos essenciais.</p>",
     resposta: true,
     dificuldade: "fácil"
   },
